dd11bd92ccfc2d161d5f633a1db6a121d6dccb1f
lc6464 <64722907+lc6464@users.noreply.github.com>
2026-02-09 20:58:38 +0800

    docs: 更新身份标识正则表达式的注释以使用继承文档，代替不起作用的自定义文档

--- GeneratedRegex.cs
+++ GeneratedRegex.cs
@@ -3,10 +3,7 @@
 namespace DeviceStatusBeacon;
 
 public static partial class GeneratedRegex {
-	/// <summary>
-	/// 获取源生成的身份标识正则表达式
-	/// </summary>
-	/// <returns>身份标识正则表达式</returns>
+	/// <inheritdoc/>
 	[GeneratedRegex(@"^[A-Za-z\d][\-A-Za-z\d]{2,62}[A-Za-z\d]$", RegexOptions.CultureInvariant | RegexOptions.Singleline)]
 	public static partial Regex IdentityRegex();
 }
